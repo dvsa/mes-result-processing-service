9ff25819253801f9d0e12625f296a69238b2e13e
ammarhaiderbjss <33695049+ammarhaiderbjss@users.noreply.github.com>
2020-02-18 10:41:19 +0000

    uses trimTestCategoryPrefix from mes-microservice-common (#52)

--- src/functions/tarsUpload/domain/util/TestCategoryConvertor.ts
+++
@@ -1,9 +0,0 @@
-import { CategoryCode } from '@dvsa/mes-test-schema/categories/common';
-
-export const convertDl25TestCategory = (category: CategoryCode): string => {
-  if (category.indexOf('EU') === 0) {
-    return category.substring(2);
-  }
-
-  return category;
-};
