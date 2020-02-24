8b32fbd497b7ec3d9788f5098bd643f846005d72
Ahmed <a.mobarak92@gmail.com>
2020-02-24 10:26:28 +0000

    MES-4924 - Improve quality of logging [3.0.5] (#53)
    * MES-4924-Improved Logging
    
    * Fix broken tests
    
    * Fixed Double JSON Stringify
    
    * Improved logging output on HTTPBatchFetcher
    
    * Added Update Upload Status Error
    
    * Added some logging for Tars Upload
    
    Co-authored-by: Tom Billington <Cavalier07@users.noreply.github.com>

--- src/functions/tarsUpload/framework/adapter/fetch/__tests__/HTTPBatchFetcher.spec.ts
+++ src/functions/tarsUpload/framework/adapter/fetch/__tests__/HTTPBatchFetcher.spec.ts
@@ -6,6 +6,7 @@ import { TYPES } from '../../../di/types';
 import { dummyResponseArray, invalidBase64Array, validBase64InvalidTestSchemna } from './dummyReponse';
 import { TestResultSchemasUnion } from '@dvsa/mes-test-schema/categories';
 import { EnvvarTestResultHTTPConfig } from '../EnvvarTestResultHTTPConfig';
+import { TestResultError } from '../errors/TestResultError';
 
 describe('HTTPBatchFetcher', () => {
   let batchFetcher: HTTPBatchFetcher;
@@ -57,9 +58,12 @@ describe('HTTPBatchFetcher', () => {
       batchFetcher.fetchNextUploadBatch().then(() => {
         fail();
       })
-        .catch((err: any) => {
-          const expectedError = new Error('Request failed with status code 500');
-          expect(err).toEqual(expectedError);
+        .catch((err: TestResultError) => {
+          expect(err.message).toEqual('Get Upload Batch failed');
+          expect(err.data).toEqual('error');
+          expect(err.method).toEqual('get');
+          expect(err.status).toEqual(500);
+          expect(err.url).toEqual('http://localhost:3001/results?interface=TARS&batch_size=25');
         });
     });
   });
@@ -74,9 +78,13 @@ describe('HTTPBatchFetcher', () => {
     it('should throw an exception', async () => {
 
       batchFetcher.fetchNextUploadBatch().then((result: TestResultSchemasUnion[]) => { fail(); })
-        .catch((err: any) => {
-          const expectedError = new Error('failed decompressing test result');
-          expect(err).toEqual(expectedError);
+        .catch((err: TestResultError) => {
+          expect(err.message).toEqual('failed decompressing test result');
+          expect(err.data).toBeUndefined();
+          expect(err.headers).toBeUndefined();
+          expect(err.method).toBeUndefined();
+          expect(err.status).toBeUndefined();
+          expect(err.url).toBeUndefined();
           return;
         });
     });
@@ -92,8 +100,12 @@ describe('HTTPBatchFetcher', () => {
     it('should throw an exception', async () => {
       batchFetcher.fetchNextUploadBatch().then((result: TestResultSchemasUnion[]) => { fail(); })
         .catch((err: any) => {
-          const expectedError = new Error('failed parsing test result');
-          expect(err).toEqual(expectedError);
+          expect(err.message).toEqual('failed parsing test result');
+          expect(err.data).toBeUndefined();
+          expect(err.headers).toBeUndefined();
+          expect(err.method).toBeUndefined();
+          expect(err.status).toBeUndefined();
+          expect(err.url).toBeUndefined();
         });
     });
   });
