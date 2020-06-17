1670cf88108958d77c92894025414231fba40102
matthew <m.bell2412@hotmail.com>
2020-06-17 12:03:03 +0100

    include CPC in the test type lookup map and update unit test

--- src/functions/tarsUpload/domain/util/TestTypeLookup.ts
+++ src/functions/tarsUpload/domain/util/TestTypeLookup.ts
@@ -17,6 +17,7 @@ export const determineDl25TestType = (category: string): number | undefined => {
        ['EUA2M1', 16], ['EUA2M2', 1],
        ['EUAM1', 16], ['EUAM2', 1],
        ['EUAMM1', 17], ['EUAMM2', 9],
+       ['CCPC', 44], ['DCPC', 44],
   ]);
   const testType = mapping.get(category);
   return testType;
