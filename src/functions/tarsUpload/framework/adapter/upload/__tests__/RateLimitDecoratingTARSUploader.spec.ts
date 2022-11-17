f3bfc14cb73a304ac2a1948f7e0900862463f7d2
andrew <andrew.setterfield@dvsa.gov.uk>
2022-11-17 10:44:21 +0000

    re-added expect

--- src/functions/tarsUpload/framework/adapter/upload/__tests__/RateLimitDecoratingTARSUploader.spec.ts
+++ src/functions/tarsUpload/framework/adapter/upload/__tests__/RateLimitDecoratingTARSUploader.spec.ts
@@ -90,8 +90,8 @@ describe('RateLimitDecoratingTARSUploader', () => {
 
       expect(approxInstantly(callTimings[0])).toEqual(true); // Second call happened instantly (different interface)
       expect(approxInstantly(callTimings[1])).toEqual(true); // Third call happened instantly (rate limit allows it)
-      // expect(approxOneSecond(callTimings[2])).toEqual(true); // Fourth call was rate limited until ~1s after
-      // expect(approxInstantly(callTimings[3])).toEqual(true);
+      expect(approxOneSecond(callTimings[2])).toEqual(true); // Fourth call was rate limited until ~1s after
+      expect(approxInstantly(callTimings[3])).toEqual(true);
       // expect(approxOneSecond(callTimings[4])).toEqual(true);
       // expect(approxInstantly(callTimings[5])).toEqual(true);
     }, 10000); // custom timeout, should be heavily overestimated
