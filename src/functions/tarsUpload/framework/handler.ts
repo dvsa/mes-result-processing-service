6bfe1d20d02ac55fe79694799a26edd07f31cd72
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2023-07-01 08:12:08 +0100

    Add calls to handler and break points (#177)

--- src/functions/tarsUpload/framework/handler.ts
+++ src/functions/tarsUpload/framework/handler.ts
@@ -1,9 +1,20 @@
 import 'reflect-metadata';
-import { Context, ScheduledEvent } from 'aws-lambda';
+import { ScheduledEvent } from 'aws-lambda';
+import { bootstrapLogging, error, info } from '@dvsa/mes-microservice-common/application/utils/logger';
 import { container } from './di/inversify.config';
 import { BatchProcessInvoker } from '../application/primary/BatchProcessInvoker';
 
-export async function handler(event: ScheduledEvent, fnCtx: Context): Promise<void> {
-  const uploadPort = container.get<BatchProcessInvoker>(BatchProcessInvoker);
-  await uploadPort.processNextBatch();
+export async function handler(event: ScheduledEvent): Promise<void> {
+  try {
+    bootstrapLogging('result-processing-service', event);
+
+    info('Calling processNextBatch');
+    const uploadPort = container.get<BatchProcessInvoker>(BatchProcessInvoker);
+    await uploadPort.processNextBatch();
+
+    info('Processed successfully');
+  } catch (err) {
+    error('### err', err);
+    throw(err);
+  }
 }
